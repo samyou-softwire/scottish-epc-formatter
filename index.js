70f84d17d67787910e490cd09481afc38da0690e
Samuel <samuel.young@softwire.com>
2024-09-12 16:28:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const csv = require('csv');
+const { program } = require('commander');
+const { readFileSync, writeFileSync } = require('fs');
+
+program.option('--infile <string>', 'infile', 'input.csv');
+program.option('--outfile <string>', 'outfile', 'output.csv');
+
+program.parse();
+
+const options = program.opts();
+
+const inFile = options.infile;
+const outFile = options.outfile;
+
+const content = readFileSync(inFile);
+
+const records = csv.parse(content, {columns: true});
+
+const outLines = [
+    'uprn,date,epc_rating'
+];
+
+records.toArray().then(arr => {
+    console.log(arr.length);
+
+    var i = 0;
+    for (const epc of arr) {
+        i++;
+        if (i == 1) continue; // first line of the file is additional headers? ignore this
+
+        outLines.push(`${epc['OSG_REFERENCE_NUMBER']},${epc['LODGEMENT_DATE']},${epc['CURRENT_ENERGY_RATING']}`);
+    }
+
+    console.log(outLines.length);
+    
+
+    writeFileSync(outFile, outLines.join("\n"));
+});
+
